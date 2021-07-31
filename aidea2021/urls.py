654dd68ce5239790b8f7d1a429cf504c1ee2778e
touchavin <touchavin@hotmail.com>
2021-07-31 16:12:08 +0700

    first commit

--- aidea2021/urls.py
+++ aidea2021/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from uploadimage.views import home
+
+
+from django.conf import settings
+from django.conf.urls.static import static 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+     path('home', home),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- uploadimage/views.py
+++ uploadimage/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request): #หน้า index.html
+       
+    return render(request, 'home.html')
