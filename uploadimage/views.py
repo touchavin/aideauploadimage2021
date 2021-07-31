c6a0f2ae35ab542cb4130e0135e61985c599782e
touchavin <touchavin@hotmail.com>
2021-07-31 16:24:02 +0700

    first commit

--- uploadimage/views.py
+++ uploadimage/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 
+from rest_framework.decorators import api_view
+from rest_framework.decorators import permission_classes
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework import status
+
+from rest_framework.status import (
+    HTTP_400_BAD_REQUEST,
+    HTTP_401_UNAUTHORIZED,
+    HTTP_404_NOT_FOUND,
+    HTTP_200_OK,
+)
 # Create your views here.
 def home(request): #หน้า index.html
        
