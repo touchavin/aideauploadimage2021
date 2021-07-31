56c370b658e0ee9a59624065320f8373fdbb96b7
touchavin <touchavin@hotmail.com>
2021-07-31 17:20:39 +0700

    first commit

--- uploadimage/admin.py
+++ uploadimage/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from.models import  Image
 
 # Register your models here.
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ['job_id', 'job_name', 'job_data', 'job_time', 'job_officerid', 'job_picture', 'vol_name', 'eq_name', 'subeq_name', 'abnor_name','abnor_other', 'nameimageold', 'nameimagenew', 'pathimage', 'show_image']
+    list_filter = ['job_name']
+    search_fields = ['job_name', 'vol_name', 'abnor_name']
+
+
+           
+
+
+admin.site.register(Image, ImageAdmin)
+
+
--- uploadimage/models.py
+++ uploadimage/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+from django.utils.html import format_html
 
 # Create your models here.
+class Image(models.Model):
+
+
+
+    job_id = models.CharField(max_length=100, blank=True, null=True)
+    job_name = models.CharField(max_length=100, blank=True, null=True)
+    job_data = models.DateField(auto_now_add=True)
+    job_time = models.TimeField(auto_now_add=True)
+    job_officerid = models.CharField(max_length=100, blank=True, null=True)
+    job_picture = models.URLField(max_length=500, blank=True, null=True)
+    vol_name = models.CharField(max_length=100, blank=True, null=True)
+    eq_name = models.CharField(max_length=100, blank=True, null=True)
+    subeq_name = models.CharField(max_length=100, blank=True, null=True)
+    abnor_name = models.CharField(max_length=100, blank=True, null=True) 
+    abnor_other = models.CharField(max_length=200, blank=True, null=True) 
+    nameimageold = models.CharField(max_length=100, blank=True, null=True)
+    nameimagenew = models.CharField(max_length=100, blank=True, null=True)
+    pathimage = models.CharField(max_length=100, blank=True, null=True) #เพิ่ม
+    image = models.FileField(upload_to='media/', null=True, blank=True)
+
+
+
+
+    # Day = models.DateField(auto_now_add=True)
+    # Time = models.TimeField(auto_now_add=True)
+    # Circuit = models.CharField(max_length=100, blank=True, null=True)
+    # Category = models.CharField(max_length=100, blank=True, null=True)
+    # Category1 = models.CharField(max_length=100, blank=True, null=True)
+    # Category2 = models.CharField(max_length=100, blank=True, null=True)
+   
+    # Accessory = models.CharField(max_length=100, blank=True, null=True)
+    # Accessory1 = models.CharField(max_length=100, blank=True, null=True)
+    # Accessory2 = models.CharField(max_length=100, blank=True, null=True)
+   
+    # Case = models.CharField(max_length=100, blank=True, null=True)
+    # Case1 = models.CharField(max_length=100, blank=True, null=True)
+    # Case2 = models.CharField(max_length=100, blank=True, null=True)
+  
+    # Customer_number = models.CharField(max_length=100, blank=True, null=True)
+    
+    
+    
+
+    def show_image(self): 
+        if self.image:
+            return format_html('<img src="%s" height="40px">' % self.image.url) 
+        return ''
+    show_image.allow_tags = True 
+    show_image.short_description = 'Image'
